b866d742fbfe802996f3397f60b1d955acd58cbc
luizAFS23 <luizafs.1500@gmail.com>
2023-12-10 14:42:02 -0300

    changes

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const main_gameboard = {
     player2: "O"
 }
 
-const displayGameboard = () =>{
+const displayGameboard = () => {
     console.log(main_gameboard.gameboard[0], main_gameboard.gameboard[1], main_gameboard.gameboard[2]);
     console.log(main_gameboard.gameboard[3], main_gameboard.gameboard[4], main_gameboard.gameboard[5]);
     console.log(main_gameboard.gameboard[6], main_gameboard.gameboard[7], main_gameboard.gameboard[8]);
@@ -24,7 +24,7 @@ const displayGameboard = () =>{
 const choosePosition = () =>{
     for(let i = 0; i <= main_gameboard.gameboard.length; i++){
         while(main_gameboard.gameboard[i] == '-'){
-            
+
         }
         rl.question("Please choose your position on the gameboard (1/9): ", function(number){
             if(main_gameboard.gameboard[number - 1] != '-'){
