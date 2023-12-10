1040b41d173be6b0d79e561fac344f5ba84db417
luizAFS23 <luizafs.1500@gmail.com>
2023-12-10 14:06:51 -0300

    changes

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const main_gameboard = {
-    gameboard: ['', '', '',
-                '', '', '',
-                '', '', ''],
-    player1: "X",
-    player2: "O"
+    gameboard: ['-', '-', '-',
+                '-', '-', '-',
+                '-', '-', '-'],
+    player1: "X"
+}
+
+const displayGameboard = () =>{
+    
 }
