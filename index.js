a47915ec1d334fdbe3a98b3684d1c10698aa15f7
luizAFS23 <luizafs.1500@gmail.com>
2023-12-10 14:13:33 -0300

    changes

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+
 const main_gameboard = {
     gameboard: ['-', '-', '-',
                 '-', '-', '-',
                 '-', '-', '-'],
-    player1: "X"
+    player1: "X",
+    player2: "O"
 }
 
 const displayGameboard = () =>{
-    
+    console.log(main_gameboard.gameboard);
+}
+
+const choosePosition = () =>{
+    console.log("Please choose your position on the gameboard ")
 }
