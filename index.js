0545ba25ac415fb3d91ee317798ef163a8f4edf6
luizAFS23 <luizafs.1500@gmail.com>
2023-12-11 10:46:11 -0300

    changes

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const displayGameboard = () => {
 }
 
 
-function theGame(){
+(function theGame(){
 
     for(let i = 0; i <= main_gameboard.gameboard.length; i++){
         const input = prompt("Please choose your position on the gameboard (1/9): ");
@@ -36,26 +36,34 @@ function theGame(){
 
     }
 
-}
-
-// function enemyAI(){
-//     for(let i = 0; i <= main_gameboard.gameboard.length; i++){
-//         let randomIndex = Math.floor(Math.random() * (main_gameboard.gameboard.length + 1));
-        
-//         if(main_gameboard.gameboard[randomIndex] == 'X') { continue; }else{
-//             main_gameboard.gameboard[randomIndex] = main_gameboard.player2;
-//         }
-//     }
-
-// }
-
-
-function runningGame(){
-    theGame();
-    if(main_gameboard.gameboard[0] == main_gameboard.gameboard[1] == main_gameboard.gameboard[2] != '-'){
-        game_on = false;
-
+    if(main_gameboard.gameboard[0] == main_gameboard.gameboard[1] == main_gameboard.gameboard[2] == 'X'){
+        console.log("Congratulations, you WON!")
+        return;
+    }else if(main_gameboard.gameboard[3] == main_gameboard.gameboard[4] == main_gameboard.gameboard[5] == 'X'){
+        console.log("Congratulations, you WON!")
+        return;
+    }else if(main_gameboard.gameboard[6] == main_gameboard.gameboard[7] == main_gameboard.gameboard[8] == 'X'){
+        console.log("Congratulations, you WON!")
+        return;
+    }else if(main_gameboard.gameboard[0] == main_gameboard.gameboard[3] == main_gameboard.gameboard[6] == 'X'){
+        console.log("Congratulations, you WON!")
+        return;
+    }else if(main_gameboard.gameboard[1] == main_gameboard.gameboard[4] == main_gameboard.gameboard[7] == 'X'){
+        console.log("Congratulations, you WON!");
+        return;
+    }else if(main_gameboard.gameboard[2] == main_gameboard.gameboard[5] == main_gameboard.gameboard[8] == 'X'){
+        console.log("Congratulations, you WON!");
+        return;
+    }else if(main_gameboard.gameboard[0] == main_gameboard.gameboard[4] == main_gameboard.gameboard[8] == 'X'){
+        console.log("Congratulations, you WON!");
+        return;
+    }else if(main_gameboard.gameboard[2] == main_gameboard.gameboard[4] == main_gameboard.gameboard[6] == 'X'){
+        console.log("Congratulations, you WON!");
+        return;
+    }else if(main_gameboard.gameboard.includes('-') == false){
+        console.log("Tie");
+    }else{
+        console.log("Sorry, you LOST!");
     }
-}
 
-runningGame();
+})();
